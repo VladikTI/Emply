0ae040b378ea35d91a1fb848db15556f1081f6e4
VladikTi <vladislav777888999@yandex.ru>
2023-10-29 17:01:12 +0300

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
+from typing import Union
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
