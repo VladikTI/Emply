8ef12ec80e341782150a0f4071ef659545c79598
Михаил Гумеров <gumerovma@my.msu.ru>
2023-11-26 23:56:05 +0300

    Добавлена регистрация и авторизация для company и hr

--- backend/src/auth/manager.py
+++ backend/src/auth/manager.py
@@ -3,7 +3,8 @@ from typing import Optional
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, IntegerIDMixin
 
-from src.auth.utils import User, get_user_db
+from src.auth.models import User, Company, HR
+from src.auth.utils import get_user_db, get_company_db, get_hr_db
 
 SECRET = "SECRET"
 
@@ -16,5 +17,29 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         print(f"User {user.id} has registered.")
 
 
+class CompanyManager(IntegerIDMixin, BaseUserManager[Company, int]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, company: Company, request: Optional[Request] = None):
+        print(f"User {company.id} has registered.")
+
+
+class HRManager(IntegerIDMixin, BaseUserManager[HR, int]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, hr: HR, request: Optional[Request] = None):
+        print(f"User {hr.id} has registered.")
+
+
 async def get_user_manager(user_db=Depends(get_user_db)):
     yield UserManager(user_db)
+
+
+async def get_company_manager(company_db=Depends(get_company_db)):
+    yield UserManager(company_db)
+
+
+async def get_hr_manager(hr_db=Depends(get_hr_db)):
+    yield UserManager(hr_db)
--- backend/src/auth/schemas.py
+++ backend/src/auth/schemas.py
@@ -1,12 +1,12 @@
 from typing import Optional
 
 from fastapi_users import schemas
+from pydantic import EmailStr
 
 
 class UserRead(schemas.BaseUser[int]):
     id: int
-    email: str
-    username: str
+    email: EmailStr
     name: Optional[str]
     surname: Optional[str]
     patronymic: Optional[str]
@@ -15,13 +15,54 @@ class UserRead(schemas.BaseUser[int]):
     is_verified: Optional[bool] = False
 
 
+class CompanyRead(schemas.BaseUser[int]):
+    id: int
+    email: EmailStr
+    name: Optional[str]
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class HRRead(schemas.BaseUser[int]):
+    id: int
+    email: EmailStr
+    name: Optional[str]
+    surname: Optional[str]
+    patronymic: Optional[str]
+    company_id: int
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
 class UserCreate(schemas.BaseUserCreate):
-    email: str
-    username: str
+    email: EmailStr
     password: str
     name: Optional[str]
     surname: Optional[str]
     patronymic: Optional[str]
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class CompanyCreate(schemas.BaseUserCreate):
+    email: EmailStr
+    password: str
+    name: Optional[str]
+    is_active: Optional[bool] = True
+    is_superuser: Optional[bool] = False
+    is_verified: Optional[bool] = False
+
+
+class HRCreate(schemas.BaseUserCreate):
+    email: EmailStr
+    password: str
+    name: Optional[str]
+    surname: Optional[str]
+    patronymic: Optional[str]
+    company_id: int
     is_active: Optional[bool] = True
     is_superuser: Optional[bool] = False
     is_verified: Optional[bool] = False
